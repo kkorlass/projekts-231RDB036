184e4acf0d4a543bbf182661c244ab68ccb55ca9
Kārlis Korlašs <85880308+kkorlass@users.noreply.github.com>
2024-01-18 18:19:50 +0200

    Pievienots bs4

--- scrape.py
+++ scrape.py
@@ -1,12 +1,17 @@
 import requests
+import sys
+from bs4 import BeautifulSoup
 
 url = 'https://phoronix.com'
 response = requests.get(url)
 
 if response.status_code == 200:
     html_content = response.text
-    print(html_content)
 else:
     print("Failed to retrieve the website")
+    sys.exit()
 
+soup = BeautifulSoup(html_content, 'html.parser')
 
+for article in soup.find_all('article'):
+    print(article)
