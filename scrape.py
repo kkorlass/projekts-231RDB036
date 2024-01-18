4fff30ac1208eec574b8a03752a226dc642caa7f
Kārlis Korlašs <85880308+kkorlass@users.noreply.github.com>
2024-01-18 18:27:11 +0200

    Iegūts raksta laiks

--- scrape.py
+++ scrape.py
@@ -14,6 +14,11 @@ else:
 soup = BeautifulSoup(html_content, 'html.parser')
 
 for article in soup.find_all('article'):
+    details_div = article.find('div', class_='details')
+    if details_div:
+        time_str = details_div.get_text().strip().split(' - ')[0]
+        print(time_str)
+    
     header = article.find('header')
     if header:
         title = header.get_text().strip()
